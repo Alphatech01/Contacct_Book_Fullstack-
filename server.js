c855ac1e82b928a0856d6c15d251366c9c522d2f
Alphatech01 <45636829+Alphatech01@users.noreply.github.com>
2025-04-09 13:51:17 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const errorHandler = require("./middleware/errorHandler");
 const connectDB = require("./connection/dbConnection");
+const cors = require("cors");
 
 
 //const contactRoutes = require('./routes/contactRoutes');
@@ -9,16 +10,15 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-
+app.use(cors());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use(errorHandler);
-const port = process.env.port  || 3000;
+// const port = process.env.PORT || 3000;
 
-
+const port = 3000;
 // // 2. Use the imported route
 // app.use("/api/contacts", contactRoutes);
-
-// const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 3000;
 app.listen(port, () =>{
         console.log(`server running in port ${port}`);
-}) 
+});
